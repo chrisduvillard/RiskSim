3644bcffa1c5215f73eb51c6ca3bc9f60e87db49
Christophe Duvillard <duvillard.c@gmail.com>
2024-06-18 17:51:57 +0200

    Refactor slider configurations in app.py

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from config.slider_configs import SLIDER_CONFIGS
+# from config.slider_configs import SLIDER_CONFIGS
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as plotly_express
@@ -7,6 +7,15 @@ from risk_simulation import TradingSimulator
 from style import footer, metric_box
 import sys
 import os
+
+SLIDER_CONFIGS = {
+    "trades_per_year": {"min_value": 5, "max_value": 100, "value": 30, "step": 5},
+    "win_rate": {"min_value": 28.0, "max_value": 70.0, "value": 40.0, "step": 2.0},
+    "risk_per_trade": {"min_value": 0.25, "max_value": 5.0, "value": 1.0, "step": 0.25},
+    "return_per_unit_risk": {"min_value": 0.5, "max_value": 8.0, "value": 3.0, "step": 0.5},
+    "num_simulations": {"min_value": 10_000, "max_value": 100_000, "value": 10_000, "step": 10_000},
+}
+
 # Add the root directory to the PYTHONPATH
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Now import the module
